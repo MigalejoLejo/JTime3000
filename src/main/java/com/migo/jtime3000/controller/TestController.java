addb87312eaf69b625545aba8a52ffa97ff3a3f7
MigalejoLejo <migalejo85@gmail.com>
2023-03-10 18:31:01 +0100

    Removed Security

--- src/main/java/com/migo/jtime3000/controller/TestController.java
+++ src/main/java/com/migo/jtime3000/controller/TestController.java
@@ -1,8 +1,16 @@
 package com.migo.jtime3000.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/testController")
 public class TestController {
+
+    @GetMapping("/hi")
+    public String sayHello(){
+        return "Hello World";
+    }
 
 }
