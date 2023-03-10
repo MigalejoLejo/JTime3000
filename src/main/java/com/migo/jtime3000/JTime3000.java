daa545a92ce4368702d0cc6216e5b0e2fe900e8a
MigalejoLejo <migalejo85@gmail.com>
2023-03-10 18:12:54 +0100

    Initial commit

---
+++ src/main/java/com/migo/jtime3000/JTime3000.java
@@ -0,0 +1,13 @@
+package com.migo.jtime3000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JTime3000 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JTime3000.class, args);
+    }
+
+}
---
+++ src/test/java/com/migo/jtime3000/JTime3000Tests.java
@@ -0,0 +1,18 @@
+package com.migo.jtime3000;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+@SpringBootTest
+class JTime3000Tests {
+
+    @Test
+    void contextLoads() {
+
+
+    }
+
+}
